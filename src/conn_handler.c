45f6e34beee478a5c97e5a92fc10586987348df0
Nick Shortway <nick@instagram.com>
2013-07-18 17:13:45 -0700

    IG specific changes

--- src/conn_handler.c
+++ src/conn_handler.c
@@ -106,7 +106,7 @@ static int stream_formatter(FILE *pipe, void *data, metric_type type, char *name
                 STREAM("%s%s.upper|%f|%lld\n", prefix, name, counter_max(value));
                 STREAM("%s%s.rate|%f|%lld\n", prefix, name, counter_sum(value) / GLOBAL_CONFIG->flush_interval);
             } else {
-                STREAM("%s%s|%f|%lld\n", prefix, name, counter_sum(value));
+                STREAM("%s%s|%f|%lld\n", prefix, name, (counter_sum(value) / GLOBAL_CONFIG->flush_interval));
             }
             break;
 
